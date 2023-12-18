edb8bef2bdbfc2c4e55a6fc9ea792d542fb4d5f4
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-12-18 17:33:12 +0300

    fix: file name

--- spec/rice_cooker_spec.rb
+++ spec/rice_cooker_spec.rb
@@ -18,21 +18,5 @@ RSpec.describe 'Rice cooker app' do
     rice_cooker.stop_cooking
     expect(rice_cooker.is_cooking).to eq(false)
   end
-
-  #it 'should cook vary gasy successfully' do
-    #rice_cooker = RiceCooker.new
-    #allow_any_instance_of(Kernel).to receive(:sleep)
-    #rice_cooker.add_water
-    #captured_output = StringIO.new
-    #$stdout = captured_output
-    #promise = rice_cooker.start_cooking('vary gasy', 3)
-
-    #sleep 3
-
-    #output_lines = captured_output.string.split("\n")
-
-    #$stdout = STDOUT
-
-    #expect(rice_cooker.has_water).to eq(false)
-  #end
+  
 end
